1a6298056419304a3387e70f5245a351b87664aa
CevaComic <46460947+CevaComic@users.noreply.github.com>
2019-12-02 12:32:36 +0200

    Add files via upload

--- html/Touch/touch.js
+++ html/Touch/touch.js
@@ -1,53 +1,15 @@
-document.getElementById('id_logic').innerHTML = "Logic Version: 2019.12.02.2"
+document.getElementById('id_logic').innerHTML = "Logic Version: 2019.12.02.4"
 
 window.addEventListener('touchstart', touch_start_uab)
-window.addEventListener('touchmove', touch_move_uab)
+window.addEventListener('touchmove', touch_move_uab, {
+	passive: false,
+})
+
 window.addEventListener('touchend', touch_end_uab)
 
 var canvas = document.getElementById('id_canvas')
 var context = canvas.getContext('2d')
 var last_position = []
-
-function touch_end_uab(p) {
-	var t = p.changedTouches
-
-	for (var i = 0; i < t.length; i++) {
-		var up_finger = -1
-		for(var j = 0; j < last_position.length ; j++)
-			if(last_position[j].id == t[i].identifier) {
-				up_finger = j;
-				break;
-			}
-		last_position.splice(up_finger, 1)
-	}
-}
-
-function touch_move_uab(p) {
-	var t = p.changedTouches
-
-	for (var i = 0; i < t.length; i++) {
-		var down_finger = -1
-		for(var j = 0; j < last_position.length ; j++)
-			if(last_position[j].id == t[i].identifier) {
-				down_finger = j;
-				break;
-			}
-		context.beginPath()
-		context.moveTo(last_position[down_finger].x,last_position[down_finger].y)
-		context.lineTo(t[i].pageX,t[i].pageY)
-		context.fillStyle = last_position[down_finger].color
-		context.strokeStyle = last_position[down_finger].color
-		context.lineWidth = 20
-		context.fill()
-		context.stroke()
-		last_position[down_finger] = {
-			...last_position[down_finger],
-			x: t[i].pageX,
-			y: t[i].pageY,
-		}
-	}
-
-}
 
 function get_random_color() {
 	// let r = Math.floor(Math.random() * 256)
@@ -61,7 +23,6 @@ function get_random_color() {
 		culoare += chars[Math.floor(Math.random() * 16)]
 	return culoare
 }
-
 
 function touch_start_uab(p) {
     var t = p.changedTouches
@@ -84,4 +45,44 @@ function touch_start_uab(p) {
 		last_position.push(touch_info)
     }
 
+}
+
+function touch_move_uab(p) {
+	p.preventDefault()
+	var t = p.changedTouches
+	for (var i = 0; i < t.length; i++) {
+		var down_finger = -1
+		for(var j = 0; j < last_position.length ; j++)
+			if(last_position[j].id == t[i].identifier) {
+				down_finger = j;
+				break;
+			}
+		context.beginPath()
+		context.moveTo(last_position[down_finger].x,last_position[down_finger].y)
+		context.lineTo(t[i].pageX,t[i].pageY)
+		context.fillStyle = last_position[down_finger].color
+		context.strokeStyle = last_position[down_finger].color
+		context.lineWidth = 20
+		context.fill()
+		context.stroke()
+		last_position[down_finger] = {
+			...last_position[down_finger],
+			x: t[i].pageX,
+			y: t[i].pageY,
+		}
+	}
+}
+
+function touch_end_uab(p) {
+	var t = p.changedTouches
+
+	for (var i = 0; i < t.length; i++) {
+		var up_finger = -1
+		for(var j = 0; j < last_position.length ; j++)
+			if(last_position[j].id == t[i].identifier) {
+				up_finger = j;
+				break;
+			}
+		last_position.splice(up_finger, 1)
+	}
 }
