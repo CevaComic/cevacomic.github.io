f887b4f1cf18be5b368ca3f893022e9c8540877c
CevaComic <46460947+CevaComic@users.noreply.github.com>
2019-12-02 11:17:11 +0200

    Add files via upload

--- html/Touch/touch.js
+++ html/Touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById('id_logic').innerHTML = "Logic Version: 2019.12.02.0"
+document.getElementById('id_logic').innerHTML = "Logic Version: 2019.12.02.1s"
 
 window.addEventListener('touchstart', touch_start_uab)
 
@@ -6,8 +6,12 @@ var canvas = document.getElementById('id_canvas')
 var context = canvas.getContext('2d')
 
 function touch_start_uab(p) {
-	var t = p.changedTouches
-	context.beginPath()
-	context.arc([t[i]].pageX,t[i].pageY,10,0,2* Math.PI)
-	context.stroke()
+    var t = p.changedTouches
+
+    for (var i = 0; i < t.length; i++) {
+        context.beginPath()
+        context.arc([t[i]].pageX, t[i].pageY, 10, 0, 2 * Math.PI)
+        context.stroke()
+    }
+
 }
